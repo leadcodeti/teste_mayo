b6c6ed133cf328f9af2fd7d9db495b63a7a287c9
HenriqueVPS <jibas1997@hotmail.com>
2022-11-29 22:54:19 -0300

    alterações de avatar

--- src/components/myAccount/index.tsx
+++ src/components/myAccount/index.tsx
@@ -1,8 +1,8 @@
 import { FiUpload, FiTrash } from "react-icons/fi";
 import Image from "next/image";
-import ImageProfile from "../../../public/images/thumbVideo.jpg";
+import defaultProfileImage from "../../../public/images/avatar.png";
 import style from "./style.module.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/useContext";
 import { useForm } from "react-hook-form";
 import { newUserDataProps } from "../../types/types";
@@ -10,6 +10,7 @@ import { ToastContainer, toast } from "react-toastify";
 
 export default function MyAccount() {
   const { user, updateUser } = useContext(AuthContext);
+  const [avatarUser, setAvatarUser] = useState(defaultProfileImage);
   const { register, handleSubmit, reset, setValue, watch } =
     useForm<newUserDataProps>();
 
@@ -22,13 +23,10 @@ export default function MyAccount() {
       avatar: data.avatar,
     };
 
-    // if (data.password == "") {
-    //   data.password = user?.password;
-    // }
-
     updateUser(dataUser);
     console.log(dataUser);
     toast.success("Seus dados Foram atualizados!");
+
     reset();
   }
 
@@ -50,18 +48,22 @@ export default function MyAccount() {
             <div className={style.imageContainer}>
               <strong>Foto de Perfil</strong>
               <Image
-                src={ImageProfile}
+                // src={user?.avatar ? "https://" + user.avatar : avatarUser}
+                src={defaultProfileImage}
                 alt="profile"
+                height={180}
+                width={200}
                 className={style.imageProfile}
               />
+              <img src={"../../../public/images/avatar.png"} alt="" />
+              {/* <input type="file" {...register("avatar")} /> */}
 
-              <input type="file" {...register("avatar")} />
               <div className={style.buttonsContainer}>
-                <button>
+                <label htmlFor="uploadAvatar">
                   <FiUpload />
                   Upload
-                </button>
-
+                </label>
+                <input type="file" id="uploadAvatar" {...register("avatar")} />
                 <button>
                   <FiTrash />
                   Remover
