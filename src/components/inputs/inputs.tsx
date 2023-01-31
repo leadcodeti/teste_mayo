632e4ddf3aea3c9495a211cd40cf010a7c192edc
Johny <johnymonteiiro@gmail.com>
2023-01-30 22:45:36 -0300

    Comments Inputs element

--- src/components/inputs/inputs.tsx
+++ src/components/inputs/inputs.tsx
@@ -1,27 +1,27 @@
 import { InputsProps } from "../../types/types";
 import style from "./style.module.scss";
 
-export function Inputs({
-  name,
-  type,
-  placeholder,
-  requiredInput,
-  defaultValue,
-}: InputsProps) {
-  return (
-    <div className={style.inputContainer}>
-      <label htmlFor={name}>
-        <span>{requiredInput}</span>
-        {name}:
-      </label>
-      <br />
-      <input
-        defaultValue={defaultValue}
-        id={name}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-      />
-    </div>
-  );
-}
+// export function Inputs({
+//   name,
+//   type,
+//   placeholder,
+//   requiredInput,
+//   defaultValue,
+// }: InputsProps) {
+//   return (
+//     <div className={style.inputContainer}>
+//       <label htmlFor={name}>
+//         <span>{requiredInput}</span>
+//         {name}:
+//       </label>
+//       <br />
+//       <input
+//         defaultValue={defaultValue}
+//         id={name}
+//         name={name}
+//         type={type}
+//         placeholder={placeholder}
+//       />
+//     </div>
+//   );
+// }
